f64d007486ddf2eaa4c7cb1b9d2a0ea309bdb71b
anegg0 <gael.blanchemain@protonmail.com>
2022-01-03 11:53:44 -0800

    added all metadata files

--- scripts/bulk-mint.js
+++ scripts/bulk-mint.js
@@ -7,21 +7,21 @@ const contractInterface = contract.abi;
 let provider = ethers.provider;
 
 const tokenURIArray = [
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
+  "https://bafkreid62y6ghstiw45ri6lckw3wvcjyfj6ufggt7xrkumf4mnv7zd5lki.ipfs.dweb.link/",
+  "https://bafkreigylhn633yfubfz7t3inovnvfzjuaol26xxrpsguefopa4gj5gx3a.ipfs.dweb.link/",
+  "https://bafkreidi3ezidml4i4dioqrcd5lbe7hl6luwc75lyr6xtizmrv3fgryfne.ipfs.dweb.link/",
+  "https://bafkreia5663fduzot2jolhvno64sv2kc2p2gb4pgbecu53hh2wi3dvyksu.ipfs.dweb.link/",
+  "https://bafkreihwlxv4vsh7inkoriaunzbgzxe54dn7psx7po6dwv5ulqvwt4jieq.ipfs.dweb.link/",
+  "https://bafkreiad7wbdtg2ukv6lepcrs2igntbsosf4cfks6ndnv2hgawbiilm32u.ipfs.dweb.link/",
+  "https://bafkreiav2o2gzi6e6izfvyblaabvuhq5jgm66g3gabhlihnnmlhu7rs72y.ipfs.dweb.link/",
+  "https://bafkreigebhwnyl7pygdhd2x3sk26bmq5ohpwbw22vfwhx4kcytfalcrdbq.ipfs.dweb.link/",
+  "https://bafkreicd2a4uiogaymvvrbdgdz23bnxnvi6f6xudeko3nbh4p2f7r4v5dm.ipfs.dweb.link/",
+  "https://bafkreigpkgdadv3xjdlfelgi2mjf7ot5wwxal5kr3meqjrubwegou56sga.ipfs.dweb.link/",
+  "https://bafkreify6o56j7qttf7bkaslvntouiodrcgiglw2or5e7snnbjhfnjurla.ipfs.dweb.link/",
+  "https://bafkreid3oepq77u7cfv2vlap64axlmmokq6lwtmb7f44blso4w6dq2qcjm.ipfs.dweb.link/",
+  "https://bafkreiepy3qyo6ydc5duuwj3oownlr47xzn7iojjmuvq3todfukx2wlllq.ipfs.dweb.link/",
+  "https://bafkreiezl3nmxnxrfhb5jlap7hju4a673clecpjse4fm375kr257qhpcj4.ipfs.dweb.link/",
+  "https://bafkreifkupbmxozavwdukqjkqjy7ht26wnmggdnf4mvrlq7hwg4odibdti.ipfs.dweb.link/",
 ];
 
 // let bulkMint = (tokenURI)=> {
